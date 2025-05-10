2912633ee175684d9d62ab87f2dd55c7f160e52b
ℂ𝕠𝕠𝕠𝕝 (𝕘𝕚𝕥𝕙𝕦𝕓.𝕔𝕠𝕞/ℂ𝕠𝕠𝕠𝕝) <coool@noreply.codeberg.org>
2025-05-10 09:32:32 +0000

    Added translation using Weblate (Latvian)

---
+++ IMSProg_editor/language/chipEditor_lv.ts
@@ -0,0 +1,265 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lv">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="14"/>
+        <source>IMSProg chip database editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="53"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="64"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="108"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="111"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="120"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="123"/>
+        <location filename="../ezp_chip_editor.ui" line="216"/>
+        <source>Ctrl+X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="132"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="135"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="144"/>
+        <source>Delete lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="147"/>
+        <source>Delete selected lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="150"/>
+        <source>Del</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="159"/>
+        <source>Add line</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="162"/>
+        <source>Add selected lines and duplicate data</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="165"/>
+        <source>Ins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="174"/>
+        <source>Move up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="177"/>
+        <source>Move lines up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="180"/>
+        <source>Ctrl+Up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="189"/>
+        <source>Move down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="192"/>
+        <source>Move lines down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="195"/>
+        <source>Ctrl+Down</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="204"/>
+        <source>Export selected lines to CSV file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="213"/>
+        <source>Export to CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="225"/>
+        <source>Import from CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.ui" line="228"/>
+        <source>, Ctrl+Shift+X</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="51"/>
+        <location filename="../ezp_chip_editor.cpp" line="53"/>
+        <location filename="../ezp_chip_editor.cpp" line="734"/>
+        <location filename="../ezp_chip_editor.cpp" line="755"/>
+        <source>Open the file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="56"/>
+        <location filename="../ezp_chip_editor.cpp" line="699"/>
+        <location filename="../ezp_chip_editor.cpp" line="737"/>
+        <location filename="../ezp_chip_editor.cpp" line="758"/>
+        <source>Current file: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="80"/>
+        <source>Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="81"/>
+        <source>Manufacturer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="82"/>
+        <source>IC Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="83"/>
+        <source>JEDEC ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="84"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="85"/>
+        <source>Sector
+size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="86"/>
+        <source>Type
+HEX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="87"/>
+        <source>Algo-
+rithm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="88"/>
+        <source>Delay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="89"/>
+        <source>4 bit
+address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="90"/>
+        <source>Block
+size K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="91"/>
+        <source>EEPROM
+pages</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="92"/>
+        <source>VCC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="530"/>
+        <location filename="../ezp_chip_editor.cpp" line="567"/>
+        <location filename="../ezp_chip_editor.cpp" line="595"/>
+        <location filename="../ezp_chip_editor.cpp" line="634"/>
+        <location filename="../ezp_chip_editor.cpp" line="674"/>
+        <location filename="../ezp_chip_editor.cpp" line="794"/>
+        <location filename="../ezp_chip_editor.cpp" line="827"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="530"/>
+        <location filename="../ezp_chip_editor.cpp" line="567"/>
+        <location filename="../ezp_chip_editor.cpp" line="595"/>
+        <location filename="../ezp_chip_editor.cpp" line="634"/>
+        <location filename="../ezp_chip_editor.cpp" line="674"/>
+        <source>No string selected.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="694"/>
+        <source>Saving file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="696"/>
+        <source>Save file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="753"/>
+        <source>Opening file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="794"/>
+        <source>Invalid CSV data file format.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ezp_chip_editor.cpp" line="827"/>
+        <source>Invalid CSV header file format.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
