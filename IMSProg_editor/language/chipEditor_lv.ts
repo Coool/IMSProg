721d6fbb95ab80ede71c03bd76197de3f66ac1f4
ℂ𝕠𝕠𝕠𝕝 (𝕘𝕚𝕥𝕙𝕦𝕓.𝕔𝕠𝕞/ℂ𝕠𝕠𝕠𝕝) <coool@noreply.codeberg.org>
2025-05-10 09:58:13 +0000

    Translated using Weblate (Latvian)
    Currently translated at 100.0% (47 of 47 strings)
    
    Translation: IMSProg/IMSProg-Editor
    Translate-URL: https://translate.codeberg.org/projects/imsprog/imsprog-editor/lv/

--- IMSProg_editor/language/chipEditor_lv.ts
+++ IMSProg_editor/language/chipEditor_lv.ts
@@ -6,128 +6,128 @@
     <message>
         <location filename="../ezp_chip_editor.ui" line="14"/>
         <source>IMSProg chip database editor</source>
-        <translation type="unfinished"></translation>
+        <translation>IMSProg mikroshēmu datubāzes redaktors</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="53"/>
         <source>File</source>
-        <translation type="unfinished"></translation>
+        <translation>Datne</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="64"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Rediģēt</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="108"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>Atvērt</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="111"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+A</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="120"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Iziet</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="123"/>
         <location filename="../ezp_chip_editor.ui" line="216"/>
         <source>Ctrl+X</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+I</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="132"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Saglabāt</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="135"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="144"/>
         <source>Delete lines</source>
-        <translation type="unfinished"></translation>
+        <translation>Dzēst rindas</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="147"/>
         <source>Delete selected lines</source>
-        <translation type="unfinished"></translation>
+        <translation>Dzēst atlasītās rindas</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="150"/>
         <source>Del</source>
-        <translation type="unfinished"></translation>
+        <translation>Del</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="159"/>
         <source>Add line</source>
-        <translation type="unfinished"></translation>
+        <translation>Pievienot rindu</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="162"/>
         <source>Add selected lines and duplicate data</source>
-        <translation type="unfinished"></translation>
+        <translation>Pievienot atlasītās rindas un dublēt datus</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="165"/>
         <source>Ins</source>
-        <translation type="unfinished"></translation>
+        <translation>Ins</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="174"/>
         <source>Move up</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot uz augšu</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="177"/>
         <source>Move lines up</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot rindas uz augšu</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="180"/>
         <source>Ctrl+Up</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Up</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="189"/>
         <source>Move down</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot uz leju</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="192"/>
         <source>Move lines down</source>
-        <translation type="unfinished"></translation>
+        <translation>Pārvietot rindas uz leju</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="195"/>
         <source>Ctrl+Down</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Down</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="204"/>
         <source>Export selected lines to CSV file</source>
-        <translation type="unfinished"></translation>
+        <translation>Izgūt atlasītās rindas uz CSV datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="213"/>
         <source>Export to CSV</source>
-        <translation type="unfinished"></translation>
+        <translation>Izgūt CSV formātā</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="225"/>
         <source>Import from CSV</source>
-        <translation type="unfinished"></translation>
+        <translation>Ievietot CSV datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.ui" line="228"/>
         <source>, Ctrl+Shift+X</source>
-        <translation type="unfinished"></translation>
+        <translation>, Ctrl+Shift+X</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="51"/>
@@ -135,7 +135,7 @@
         <location filename="../ezp_chip_editor.cpp" line="734"/>
         <location filename="../ezp_chip_editor.cpp" line="755"/>
         <source>Open the file</source>
-        <translation type="unfinished"></translation>
+        <translation>Atvērt datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="56"/>
@@ -143,78 +143,84 @@
         <location filename="../ezp_chip_editor.cpp" line="737"/>
         <location filename="../ezp_chip_editor.cpp" line="758"/>
         <source>Current file: </source>
-        <translation type="unfinished"></translation>
+        <translation>Pašreizējā datne: </translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="80"/>
         <source>Type</source>
-        <translation type="unfinished"></translation>
+        <translation>Tips</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="81"/>
         <source>Manufacturer</source>
-        <translation type="unfinished"></translation>
+        <translation>Ražotājs</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="82"/>
         <source>IC Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Integrētās mikroshēmas nosaukums</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="83"/>
         <source>JEDEC ID</source>
-        <translation type="unfinished"></translation>
+        <translation>JEDEC ID</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="84"/>
         <source>Size</source>
-        <translation type="unfinished"></translation>
+        <translation>Izmērs</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="85"/>
         <source>Sector
 size</source>
-        <translation type="unfinished"></translation>
+        <translation>Sektora
+izmērs</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="86"/>
         <source>Type
 HEX</source>
-        <translation type="unfinished"></translation>
+        <translation>Tips
+HEX</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="87"/>
         <source>Algo-
 rithm</source>
-        <translation type="unfinished"></translation>
+        <translation>Algo-
+ritms</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="88"/>
         <source>Delay</source>
-        <translation type="unfinished"></translation>
+        <translation>Aizkave</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="89"/>
         <source>4 bit
 address</source>
-        <translation type="unfinished"></translation>
+        <translation>4 bitu
+adrese</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="90"/>
         <source>Block
 size K</source>
-        <translation type="unfinished"></translation>
+        <translation>Bloka
+izmērs K</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="91"/>
         <source>EEPROM
 pages</source>
-        <translation type="unfinished"></translation>
+        <translation>EEPROM
+lapas</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="92"/>
         <source>VCC</source>
-        <translation type="unfinished"></translation>
+        <translation>VCC</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="530"/>
@@ -225,7 +231,7 @@ pages</source>
         <location filename="../ezp_chip_editor.cpp" line="794"/>
         <location filename="../ezp_chip_editor.cpp" line="827"/>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Brīdinājums</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="530"/>
@@ -234,32 +240,32 @@ pages</source>
         <location filename="../ezp_chip_editor.cpp" line="634"/>
         <location filename="../ezp_chip_editor.cpp" line="674"/>
         <source>No string selected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nav atlasīta neviena rinda.</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="694"/>
         <source>Saving file</source>
-        <translation type="unfinished"></translation>
+        <translation>Saglabā datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="696"/>
         <source>Save file</source>
-        <translation type="unfinished"></translation>
+        <translation>Saglabāt datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="753"/>
         <source>Opening file</source>
-        <translation type="unfinished"></translation>
+        <translation>Atver datni</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="794"/>
         <source>Invalid CSV data file format.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederīgs CSV datnes formāts.</translation>
     </message>
     <message>
         <location filename="../ezp_chip_editor.cpp" line="827"/>
         <source>Invalid CSV header file format.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederīgs CSV galvenes datnes formāts.</translation>
     </message>
 </context>
 </TS>
